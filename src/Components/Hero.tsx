b4bf589325bef8bc3d549d0a5d94a2cf40b7521c
MD NURULLAH BHUIYAN <121657120+nurullah91@users.noreply.github.com>
2024-08-17 01:11:12 +0600

    swiper added on hero section

--- src/Components/Hero.tsx
+++ src/Components/Hero.tsx
@@ -1,14 +1,43 @@
 import React from "react";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/effect-fade";
+import { EffectFade, Autoplay } from "swiper/modules";
+
+import "./Hero.css";
+import banner1 from "../assets/Banner-1.jpg";
+import banner2 from "../assets/Banner-2.jpg";
+
 const Hero: React.FC = () => {
   return (
     <div>
-      <div className="hero-section h-[250px] py-5 text-red-500 text-center flex flex-col items-center justify-center">
-        <h1 className="text-3xl font-bold">তথ্য ও যোগাযোগ প্রযুক্তি বিভাগ</h1>
-        <h1 className="text-2xl font-semibold">
-          গণপ্রজাতন্ত্রী বাংলাদেশ সরকার
-        </h1>
-      </div>
+      <Swiper
+        spaceBetween={30}
+        effect={"fade"}
+        autoplay={{
+          delay: 2500,
+          disableOnInteraction: false,
+        }}
+        modules={[EffectFade, Autoplay]}
+        className="mySwiper"
+      >
+        <SwiperSlide>
+          <img src={banner1} />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={banner2} />
+        </SwiperSlide>
+      </Swiper>
     </div>
+    // <div>
+    //   <div className="hero-section h-[250px] py-5 text-red-500 text-center flex flex-col items-center justify-center">
+    //     <h1 className="text-3xl font-bold">তথ্য ও যোগাযোগ প্রযুক্তি বিভাগ</h1>
+    //     <h1 className="text-2xl font-semibold">
+    //       গণপ্রজাতন্ত্রী বাংলাদেশ সরকার
+    //     </h1>
+    //   </div>
+    // </div>
   );
 };
 
